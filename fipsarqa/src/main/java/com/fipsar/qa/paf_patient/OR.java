e9c35b3869ea020b241e84e8203f43ec175b18ea
Automation_Test <sbabupresidency@gmail.com>
2018-09-11 13:28:49 +0530

    asfdg

--- fipsarqa/src/main/java/com/fipsar/qa/paf_patient/OR.java
+++ fipsarqa/src/main/java/com/fipsar/qa/paf_patient/OR.java
@@ -3,7 +3,7 @@ package com.fipsar.qa.paf_patient;
 public interface OR {
 
 	public final String PATIENT_USERNAME="//input[@type='email']";
-	public final String PATIENT_PASSWORD="//input[@type='password']";
+	public final String PATIENT_PASSWORD="//input[@id='password']";
 	public final String PATIENT_SUBMIT="//button[text()='Submit']";
 	public final String PATIENT_LOGIN_USERNAME_REQUIRED="//span[contains(text(),'User Name')]";
 	public final String PATIENT_LOGIN_PASSWORD_REQUIRED="//span[contains(text(),'Password')]";
